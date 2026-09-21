42a5be4c230eb7a94c69fa411f8dd186bb4d5442
Jerjes Mariluz <juniormariluz1@gmail.com>
2026-09-20 23:56:19 -0500

    fix: update query in projects model to use service_project table.

--- src/models/projects.js
+++ src/models/projects.js
@@ -6,16 +6,15 @@ const getAllProjects = async () => {
     SELECT
       p.project_id,
       p.organization_id,
-      p.category_id,
       p.title,
       p.description,
       p.location,
-      p.date,
+      p.project_date AS date,
       o.name AS organization_name
-    FROM public.project p
-    INNER JOIN public.organization o
+    FROM service_project p
+    INNER JOIN organization o
       ON p.organization_id = o.organization_id
-    ORDER BY p.date ASC;
+    ORDER BY p.project_date ASC;
   `;
 
   const result = await db.query(query);
